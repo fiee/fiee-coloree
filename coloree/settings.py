10750ebfe1ad5f682210c4176f58c11d86d47f62
Henning Hraban Ramm <hraban@fiee.net>
2011-10-07 10:46:21 +0200

    add locales

---
+++ coloree/settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from django.utils.translation import ugettext_lazy as _
+_ = lambda s: s # don’t import settings!
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+TIME_ZONE = 'Europe/Zurich'
+LANGUAGE_CODE = 'de'
+LANGUAGES = (('de', _(u'German')),
+             ('en', _(u'English')),
+             )
+
+USE_I18N = True
+USE_L10N = True
